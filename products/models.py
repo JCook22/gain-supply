2058bba623c25959b94e224265b454d80a327622
Jordan Cook <jordancook94@live.co.uk>
2024-07-31 00:01:10 +0000

    Create product details page and link

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
 
     class Meta:
@@ -26,4 +27,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
