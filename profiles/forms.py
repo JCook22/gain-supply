b426843d155e6ef7d6414a4a400f618d6e3138c4
Jordan Cook <jordancook94@live.co.uk>
2024-08-15 17:16:48 +0000

    Finish form to update default details. Update success toast to remove bag info

---
+++ profiles/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postcode',
+            'default_town_or_city': 'Town / City',
+            'default_street_address1': 'Address Line 1',
+            'default_street_address2': 'Address Line 2',
+            'default_county': 'County / State / Locality',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'profile-form-input rounded-0 border-black'
+            self.fields[field].label = False
