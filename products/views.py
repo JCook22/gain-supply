d21e27871869972cb98f17b3f89880f3e1390ab8
Jordan Cook <jordancook94@live.co.uk>
2024-08-16 17:34:56 +0000

    Add login_required to profile, add edit and delete product for superusers only

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category
@@ -65,7 +66,12 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
+    if not request.user.is_superuser:
+        messages.error(request, 'You are not authorised to do that.')
+        return redirect(reverse('home'))
+
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -82,7 +88,13 @@ def add_product(request):
     }
     return render(request, template, context)
 
+
+@login_required
 def edit_product(request, product_id):
+    if not request.user.is_superuser:
+        messages.error(request, 'You are not authorised to do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -103,8 +115,13 @@ def edit_product(request, product_id):
     }
     return render(request, template, context)
 
-    
+
+@login_required
 def delete_product(request, product_id):
+    if not request.user.is_superuser:
+        messages.error(request, 'You are not authorised to do that.')
+        return redirect(reverse('home'))
+        
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product successfully deleted.')
--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from checkout.models import Order
 from .forms import UserProfileForm
 
 
+@login_required
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     if request.method == 'POST':
@@ -26,6 +28,8 @@ def profile(request):
 
     return render(request, template, context)
 
+
+@login_required
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
     messages.info(request, (
