0cd0d9337a606c93a93872216598ae9e8d50b9cf
Jordan Cook <jordancook94@live.co.uk>
2024-08-16 21:25:55 +0000

    Finish work on edit and add product page. Write javascript to handle image filename changes. Add custom widgets

---
+++ products/widgets.py
@@ -0,0 +1,9 @@
+from django.utils.translation import gettext_lazy as _
+from django.forms.widgets import ClearableFileInput
+
+
+class CustomClearableFileInput(ClearableFileInput):
+    clear_checkbox_label = _('Remove')
+    input_text = _('')
+    initial_text = _('Current Image')
+    template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
