1442d149c066aefa54e1df39a90d402c6c9afb42
Jordan Cook <jordancook94@live.co.uk>
2024-08-17 20:37:50 +0000

    Fix typo in testimonial model

--- testimonials/models.py
+++ testimonials/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Testimonial(models.Model):
     customer_name = models.CharField(max_length=254) 
-    customer_age = models.IntegerField(max_digits=3, null=True, blank=True)
+    customer_age = models.IntegerField(null=True, blank=True)
     customer_county = models.CharField(max_length=80, null=True, blank=True)
     rating = models.DecimalField(max_digits=2, decimal_places=1)
-    headline = models.Charfield(max_length=254)
+    headline = models.CharField(max_length=254)
     body = models.TextField()
 
     def __str__(self):
