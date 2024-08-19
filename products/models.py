3e4b596d844805b939a368f98b45aad6b04b93ee
Jordan Cook <jordancook94@live.co.uk>
2024-08-19 21:21:15 +0000

    Make product rating no longer required and migrate change

--- products/models.py
+++ products/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     shoe_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
