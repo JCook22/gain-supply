042ca7d0765ef1dde9b5ae267c5103c98d02d5d8
Jordan Cook <jordancook94@live.co.uk>
2024-08-26 01:42:29 +0000

    Update for pep8 compliance

--- checkout/models.py
+++ checkout/models.py
@@ -88,7 +88,7 @@ class Order(models.Model):
                 self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
             )
         else:
-            self.delivery_cost = 0
+            self.delivhoery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
